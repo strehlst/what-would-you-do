8bbb55ac9453b174a8721648a2dbd157ba258248
strehlst <strehl@szenar.io>
2018-03-07 01:43:09 +0000

    Add migration for empty string as email default value on User records

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180307010518) do
+ActiveRecord::Schema.define(version: 20180307012256) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 20180307010518) do
 
   create_table "users", force: :cascade do |t|
     t.string "public_name", null: false
-    t.string "email"
+    t.string "email", default: ""
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "encrypted_password", default: "", null: false
