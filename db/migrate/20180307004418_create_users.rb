b93d4ff36e223a0750ac242d2512b234affa16ca
strehlst <strehl@szenar.io>
2018-03-07 00:48:38 +0000

    Add User model and migrations

---
+++ db/migrate/20180307004418_create_users.rb
@@ -0,0 +1,10 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :public_name, null: false
+      t.string :email
+
+      t.timestamps null: false
+    end
+  end
+end
