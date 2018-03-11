6cc360f89ef0b5cfb6e04ebda30969901a575a2b
strehlst <strehl@szenar.io>
2018-03-11 20:04:58 +0000

    Set application locale via environment variable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,17 @@ class ApplicationController < ActionController::Base
   before_action :set_locale
 
   protect_from_forgery with: :exception
+
+  private
+
+  def set_locale
+    requested_locale = ENV['APPLICATION_LOCALE']
+
+    unless requested_locale && I18n.available_locales.include?(requested_locale.to_sym)
+      requested_locale = I18n.default_locale
+    end
+
+    session[:locale] = requested_locale
+    I18n.locale      = requested_locale
+  end
 end
