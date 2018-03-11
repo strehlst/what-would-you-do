66638db15330ee2074a8dd54327230b8b4b9b999
Steven Strehl <strehlst@users.noreply.github.com>
2018-03-11 23:00:58 +0000

    [29] Devise cancel registration view markup (#39)
    * Update edit registrations view and i18n keys
    
    * Fix Rubocop offenses

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   root 'pages#frontpage'
 
   devise_for :users, controllers: { registrations: 'registrations' }
-  resources :users, only: %i[show]
   get 'user/dreams', to: 'users#dreams'
+  resources :users, only: %i[show]
   resources :dreams, only: %i[index show new create]
   get 'dreams/:id/embrace', to: 'dreams#embrace', as: 'embrace_dream'
   delete 'dreams/:id/disembrace', to: 'dreams#disembrace', as: 'disembrace_dream'
