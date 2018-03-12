b68124a5f6d4962f886987abe94e30d10fd9b9bb
strehlst <strehl@szenar.io>
2018-03-12 13:27:01 +0000

    Database scheme migrations

---
+++ db/migrate/20180312131638_add_avatar_path_to_users.rb
@@ -0,0 +1,9 @@
+class AddAvatarPathToUsers < ActiveRecord::Migration[5.1]
+  def up
+    add_column :users, :avatar_path, :string, null: true, unique: true
+  end
+
+  def down
+    remove_column :users, :avatar_path
+  end
+end
