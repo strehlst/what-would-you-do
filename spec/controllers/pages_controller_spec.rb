8d267b83da449543a9cc6860d54f2e4572afebb6
Steven Strehl <strehlst@users.noreply.github.com>
2018-03-21 02:45:09 +0000

    [72] Catch double embraces (#73)
    * Add specs for PagesController
    
    * Use http status matcher
    
    * Add specs for DreamsController

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,5 +2,33 @@
 
 require 'rails_helper'
 
-RSpec.describe PagesController, type: :controller do
+describe PagesController, type: :controller do
+  context 'GET #about' do
+    it 'renders the about template' do
+      get :about
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template('about')
+    end
+  end
+  context 'GET #contact' do
+    it 'renders the contact template' do
+      get :contact
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template('contact')
+    end
+  end
+  context 'GET #frontpage' do
+    it 'renders the about frontpage' do
+      get :frontpage
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template('frontpage')
+    end
+  end
+  context 'GET #not_found' do
+    it 'renders the not_found template' do
+      get :not_found
+      expect(response).to have_http_status(:not_found)
+      expect(response).to render_template('not_found')
+    end
+  end
 end
