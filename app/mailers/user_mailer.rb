8bb07316aee3abb22f476a6b2e612e14d88355b0
strehlst <strehl@szenar.io>
2018-03-21 22:04:54 +0000

    Disable UserMailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 class UserMailer < ApplicationMailer
+=begin
   def dream_created(caption)
     @message = caption
     mail(to: 'your-email@example.com',
          subject: 'A new dream has been imagined...')
   end
+=end
 end
--- spec/mailers/previews/user_mailer_preview.rb
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -2,7 +2,9 @@
 
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
+=begin
   def dream_created
     UserMailer.dream_created('pay back my student loan')
   end
+=end
 end
