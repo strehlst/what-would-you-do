5ee8fbd358316e42de70ff31e6c15d9c27e38b3d
strehlst <strehl@szenar.io>
2018-03-21 22:06:13 +0000

    Add ImageHelper back

---
+++ app/helpers/image_helper.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module ImageHelper
+  def quadratic_image_path(image_key, size = nil)
+    cloudinary_image(image_key, size)
+  end
+
+  private
+
+  def cloudinary_image(image_key, size = nil)
+    if size.nil?
+      cl_image_path(image_key)
+    else
+      cl_image_path(image_key, gravity: 'face', crop: 'fill', height: size, width: size)
+    end
+  end
+end
