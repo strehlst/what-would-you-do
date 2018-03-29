a20e61d3b2dba17f8026f5837e19a0c07012cd3c
Steven Strehl <strehlst@users.noreply.github.com>
2018-03-29 20:41:23 +0100

    [FIX] Add JS file to asset pipeline #106

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -14,4 +14,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
-Rails.application.config.assets.precompile += %w[normalize.css]
+Rails.application.config.assets.precompile += %w[document_ready.js normalize.css]
