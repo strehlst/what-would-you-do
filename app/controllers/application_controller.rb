e8ee5f9ea5c209cab37cc3198b39cd1ef07a169e
Steven Strehl <strehlst@users.noreply.github.com>
2018-03-31 01:05:21 +0100

    [111] Redirects after sign in and sign out (#112)
    * Fix missing dream bug
    
    * Set redirects after sign_in, sign_out and non-granted access
    
    * Update translations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,16 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
+  protected
+
+  def after_sign_in_path_for(resource)
+    request.env['omniauth.origin'] || stored_location_for(resource) || user_dreams_path
+  end
+
+  def after_sign_out_path_for(*)
+    dreams_path
+  end
+
   private
 
   def set_locale
