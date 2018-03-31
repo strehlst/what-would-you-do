9e3ecf7e4b1f4dea66595f97df7c177e66158c7b
strehlst <strehl@szenar.io>
2018-03-31 01:04:01 +0100

    Fix Rubocop offense

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,7 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     request.env['omniauth.origin'] || stored_location_for(resource) || user_dreams_path
   end
+
   def after_sign_out_path_for(*)
     dreams_path
   end
