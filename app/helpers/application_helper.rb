a33abd0b39c3a8c139f7efb28f0567ead596919b
strehlst <strehl@szenar.io>
2018-04-11 02:49:20 +0100

    Initialize variable with empty string

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,7 @@
 
 module ApplicationHelper
   def page_title
+    page_title = ''
     # If instance variable from controller action sets a title, use it
     page_title = @page_title + ' | ' if @page_title
     page_title + t('main.unconditional_basic_income')
